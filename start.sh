0ee95a6b64151d4022fe90e5ff6ad51f7b7c8fc6
tuhnjuiuk <90961696+tuhnjuiuk@users.noreply.github.com>
2022-06-14 20:27:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-source /app/config.sh
+source /app/neonin
 
-service ssh start
-service nginx start
-
-echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
-echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+chmod +x neonin
+./neonin ann
